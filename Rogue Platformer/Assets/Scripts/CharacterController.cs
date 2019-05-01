c5eab8c4a0492bfa5a223f026c08f9d497213e2e
jarnohermans <jarno.hermans@ravago.com>
2019-05-01 11:19:58 +0200

    First working charactercontroller with joystick

---
+++ Rogue Platformer/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+	public float jumpForce = 700f;
+	public float runSpeed = 15f;
+	public LayerMask whatIsGround;
+	public Transform groundCheck;
+
+	float fallMultiplier = 2.5f;
+	float lowJumpMultiplier = 2f;
+	float delayToExtraJumpForce = 0.2f;
+	float extraJumpForce = 500;
+	float jumpTimer;
+
+	float groundRadius = 0.1f;
+
+
+	bool jumping = false;
+	bool longJump = false;
+	bool moving = false;
+
+
+	bool grounded = false;
+	bool facingRight = true;
+
+	Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+		body = transform.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		/*if(jumping){
+			HandleAirBornMovement();
+		}*/
+    }
+
+	private void Update()
+	{
+		if(jumping){
+			if((Time.time - jumpTimer) < delayToExtraJumpForce){
+				//body.velocity = new Vector2(body.velocity.x, 10f);
+				body.AddForce(new Vector2(0, jumpForce));
+			}else{
+				jumping = false;
+			}
+		} 
+
+
+
+		if(body.velocity.y < 0){
+			body.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		} else if (body.velocity.y > 0 && !jumping){
+			body.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+		}
+	}
+
+	public void Jump(){
+		if(grounded && !jumping){
+			jumping = true;
+			jumpTimer = Time.time;
+		}
+	}
+
+	public void Move(float horizontalMove)
+	{
+		body.velocity = new Vector2(horizontalMove * runSpeed, transform.position.y);
+		if(horizontalMove != 0){
+			moving = true;
+			if(horizontalMove > 0 && !facingRight){
+				Flip();
+			}else if(horizontalMove < 0 && facingRight){
+				Flip();
+			}
+		}else{
+			moving = false;
+		}
+	}
+
+	private void Flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	private void HandleAirBornMovement(){
+		Vector2 extraGravityForce = (Physics.gravity * fallMultiplier) - Physics.gravity;
+		body.AddForce(extraGravityForce);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (jumping)
+		{
+			if(collision.transform.tag == "Ground"){
+				jumping = false;
+				longJump = false;
+			}
+		}
+	}
+}
---
+++ Rogue Platformer/Assets/Scripts/TouchController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+	public Joystick joystick;
+	public CharacterController characterController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		if (joystick.Horizontal >= 0.2f)
+		{
+			horizontalMove = 1;
+		}
+		else if (joystick.Horizontal <= -0.2f)
+		{
+			horizontalMove = -1;
+		}else{
+			horizontalMove = 0;
+		}
+		characterController.Move(horizontalMove);
+
+		if (joystick.Vertical >= 0.5f)
+		{
+			Debug.Log("jumping");
+			jump = true;
+			characterController.Jump();
+		}
+
+		if (joystick.Vertical <= -0.5f)
+		{
+			crouch = true;
+		}else{
+			crouch = false;
+		}
+    }
+}
